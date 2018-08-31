17f2604d4fd512ca07376ccff97a2eb3f3f093a2
J Nicholson <jenicholson.nc@gmail.com>
2018-08-31 15:09:52 +0200

    add converter for text from the created dictionary

--- public/javascripts/converter.js
+++ public/javascripts/converter.js
@@ -1,10 +1,16 @@
 $('.text-convert').click(e => {
   e.preventDefault();
   //console.log($('textarea[name=received-text]').val());
-  let text = $('textarea[name=received-text]').val();
-  console.log(text);
-  let entries = dictionaryApi.get('');
-  for (i = 0; i < text.length; i++) {
-    // if (textArray[i] === dictionaryApi)
-  }
+  dictionaryApi.get('').then(response => {
+    let text = $('textarea[name=received-text]').val();
+    for (let i = 0; i < response.data.length; i++) {
+      var originalWord = response.data[i].originalWord;
+      var convertedWord = response.data[i].convertedWord;
+      var convertedText = text.replace(originalWord, convertedWord);
+      text = convertedText;
+    }
+    console.log('new text:', text);
+    $('.converted-header').html('Fixed it!');
+    $('.converted-text').html(text);
+  });
 });
